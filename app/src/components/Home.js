a5a7840aa29cbf773c7eccb190a5a79f3e7b5dcc
Dracgamer5643 <supermee5643@gmail.com>
2025-01-15 20:22:50 +0530

    Added some css for the logo nothing much

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -5,6 +5,8 @@ export default function Home() {
     return (
         <div className='mainCon'>
             <div className="form">
+                <h1><img src="./logo.png" alt="logo" />Auto<span>Grade</span></h1>
+                <p>Streamlined Grading, Smarter Learning.</p>
                 <label htmlFor="ansSheetImage">Upload Answer Sheet</label>
                 <input type="file" id="ansSheetImage"/>
                 <br />
